acad01c6ae53ad32f61b9adec8adf21d418deea7
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-03-13 12:10:13 +0700

    one

--- alll/10.py
+++ alll/10.py
@@ -1,19 +1,8 @@
-def can_gears_rotate(num_gears, gear_teeth):
-    total = sum(gear_teeth)
-    if total == 0:
-        return True
+def number_of_gears(n):
+    if n % 2 == 0:
+        return "Шестеренки могут вращаться"
     else:
-        return False
+        return "Шестеренки не могут вращаться"
 
-# Пример использования программы
-num_gears = int(input("Введите количество шестеренок: "))
-gear_teeth = []
-
-for i in range(2):
-    teeth = int(input(f"Введите количество зубьев для шестеренки {i+1}: "))
-    gear_teeth.append(teeth)
-
-if can_gears_rotate(num_gears, gear_teeth):
-    print("Шестерёнки могут вращаться")
-else:
-    print("Шестерёнки не могут вращаться")
+n = int(input("Введите количество шестеренок в последовательности: "))
+print(number_of_gears(n))
