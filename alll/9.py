418c59ef93b964fbeed67d7de13316ab91dd6c9f
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-03-13 17:46:44 +0700

    one

--- alll/9.py
+++ alll/9.py
@@ -3,8 +3,29 @@ if dog=='да':
     print('Рост собаки менее 50 см?')
 if dog=='нет':
     print('Рост собаки менее 50 см?')
+ask=input()
+if ask== 'нет':
+        print('Собака весит более 50 кг?')
+weight=input()
+if weight=='нет':
+    print('фоксхаунд')
+if weight=='да':
+    print('датский дог')
 height=input()
 if height=='да':
     print('У собаки короткий хвост?')
-    if height=='нет':
-        print('У собаки длинные уши?')
+tail=input()
+if tail=='да':
+    print('английский бульдог')
+if tail=='нет':
+    print('У собаки длинные уши?')
+ears=input()
+if ears=='да':
+    print('гончая')
+if ears=='нет':
+    print('У собаки короткое тело?')
+body=input()
+if body=='да':
+    print('мопс')
+if body=='нет':
+    print('чихуахуа')
