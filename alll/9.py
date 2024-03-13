13a9e996aed7d7c92298a9d7d36a50a6f1ba329f
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-03-13 20:32:11 +0700

    one

--- alll/9.py
+++ alll/9.py
@@ -1,31 +1,50 @@
-dog=input('Собака короткошерстной породы?')
-if dog=='да':
-    print('Рост собаки менее 50 см?')
-if dog=='нет':
-    print('Рост собаки менее 50 см?')
-ask=input()
-if ask== 'нет':
-        print('Собака весит более 50 кг?')
-weight=input()
-if weight=='нет':
-    print('фоксхаунд')
-if weight=='да':
-    print('датский дог')
-height=input()
-if height=='да':
-    print('У собаки короткий хвост?')
-tail=input()
-if tail=='да':
-    print('английский бульдог')
-if tail=='нет':
-    print('У собаки длинные уши?')
-ears=input()
-if ears=='да':
-    print('гончая')
-if ears=='нет':
-    print('У собаки короткое тело?')
-body=input()
-if body=='да':
-    print('мопс')
-if body=='нет':
-    print('чихуахуа')
+dog = input('Собака короткошерстной породы?')
+if dog == 'да':
+    height = input('Рост собаки менее 50 см?')
+    if height == 'да':
+        tail = input('У собаки короткий хвост?')
+        if tail == 'да':
+            print('Английский бульдог')
+        else:
+            ears = input('У собаки длинные уши?')
+            if ears == 'да':
+                print('Гончая')
+            else:
+                body = input('У собаки короткое тело?')
+                if body == 'да':
+                    print('Мопс')
+                else:
+                    print('Чихуахуа')
+    else:
+        weight = input('Собака весит более 50 кг?')
+        if weight == 'да':
+            print('Датский дог')
+        else:
+            print('Фоксхаунд')
+else:
+    height_1 = input('Рост собаки менее 50 см?')
+    if height_1 == 'да':
+        personality = input('У собаки доброжелательный характер? (Да/Нет)')
+        if personality.lower() == 'да':
+            print('Кокер-спаниэль')
+        else:
+            print('Ирландский сеттер')
+    else:
+        height_2 = input('У собаки рост менее 70 см?')
+        if height_2 == 'да':
+            ears_2 = input('У собаки длинные уши?')
+            if ears_2 == 'да':
+                print('Большой вандейский грифон')
+            else:
+                print('Колли')
+        else:
+            color = input('Окрас рыжий с белыми отметинами?')
+            if color == 'да':
+                print('Сенбернар')
+            else:
+                coloring = input('Белоснежный окрас?')
+                if coloring == 'да':
+                    print('Ирландский волкодав')
+                else:
+                    print('Ньюфаундленд')
+
